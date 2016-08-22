14b1797226c997c489982b457f6e5687f5ce1f18
Savera <Level 2>
2016-08-22 12:00:15 -0700

    cool stuff

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -5,11 +5,18 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class MyFirstJavaProgram {
 	public static void main(String[] args) {
-		
-		// START HERE
 
-	
+	Robot steve = new Robot();
+steve.sp
+	steve.penDown();
+		steve.setPenColor(000,000,238);
+steve.moveTo(500,500);
+steve.move(200);
+		steve.turn(90);
+		steve.move(400);
+	steve.turn(90);
+		steve.move(200);
+		steve.turn(90);
 		
 		
-	}
-}
+	}}
---
+++ src/day1/robot/diamonds.java
@@ -0,0 +1,19 @@
+package day1.robot;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class diamonds {
+public static void main(String[] args) {
+	Robot gold = new Robot("batman");
+	for (int i = 0; i < 4; i++) {
+gold.penDown();
+gold.move(200);
+gold.turn(90);
+
+
+
+
+
+	}
+}
+}
